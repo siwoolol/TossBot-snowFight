d3f25e6e05e0e28c7f6c8f1d2ae99909214cb4ae
siwoo <jamieyun06@gmail.com>
2024-12-20 23:19:26 +0900

    fix float

--- Bot.py
+++ Bot.py
@@ -13,8 +13,8 @@ import win32api, win32con
 def click(x,y):
     win32api.SetCursorPos((x,y))
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,0,0)
-    rr = random.randint(0.03,0.06)
-    time.sleep(rr)
+    rr = random.randint(3,6)
+    time.sleep(rr/100)
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,0,0)
 
 while keyboard.is_pressed('q') == False:
