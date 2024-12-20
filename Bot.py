5a8f50555f63eb9e6600bdeeb6167e49d04cc39e
siwoo <jamieyun06@gmail.com>
2024-12-20 23:22:55 +0900

    latency

--- Bot.py
+++ Bot.py
@@ -13,22 +13,23 @@ import win32api, win32con
 def click(x,y):
     win32api.SetCursorPos((x,y))
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,0,0)
-    rr = random.randint(3,6)
     time.sleep(rr/100)
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,0,0)
 
 while keyboard.is_pressed('q') == False:
+    rr = random.randint(3, 6)
+
     if pyautogui.pixel(92, 769)[0] == 0:
         click(92, 769)
         print("r(0) detected in (92, 769)")
-        print("object clicked")
+        print(f"object clicked: took {rr*10}ms")
 
     elif pyautogui.pixel(254, 769)[0] == 0:
         click(254, 769)
         print("r(0) detected in (254, 769)")
-        print("object clicked")
+        print(f"object clicked: took {rr*10}ms")
 
     elif pyautogui.pixel(423, 769)[0] == 0:
         click(423, 769)
         print("r(0) detected in (423, 769)")
-        print("object clicked")
+        print(f"object clicked: took {rr*10}ms")
